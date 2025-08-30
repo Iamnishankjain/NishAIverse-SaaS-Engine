e9229170634a2d5195e5422ed7bdd7400ffe6802
Iamnishankjain <jnishank123@gmail.com>
2025-08-30 19:05:46 +0530

    review resume complete

--- frontend/src/pages/ReviewResume.jsx
+++ frontend/src/pages/ReviewResume.jsx
@@ -1,12 +1,44 @@
 import { FileText, Sparkles } from 'lucide-react';
 import React, { useState } from 'react'
+import toast from "react-hot-toast";
+import Markdown from "react-markdown";
+import axios from "axios";
 
+axios.defaults.baseURL = import.meta.env.VITE_BASE_URL;
 
 const ReviewResume = () => {
   const [input,setInput] = useState('');
+
+  const [loading, setLoading] = useState(false);
+  const [content, setContent] = useState("");
+
+  const { getToken } = useAuth();
     
   const onSubmitHandler = async (e) =>{
     e.preventDefault();
+    try {
+      setLoading(true);
+
+      const formData = new FormData();
+      formData.append("resume", input);
+
+      const { data } = await axios.post(
+        "/api/ai/resume-review",
+        formData,
+        {
+          headers: { Authorization: `Bearer ${await getToken()}` },
+        }
+      );
+
+      if (data.success) {
+        setContent(data.content);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (err) {
+      toast.error(err.message);
+    }
+    setLoading(false);
   }
   return (
     <div className='h-full overflow-y-scroll p-6 flex items-start flex-wrap gap-4 text-slate-400'>
@@ -21,8 +53,12 @@ const ReviewResume = () => {
 
         <p className='text-xs text-gray-500 font-light mt-1'>Supports Pdf format only</p>
       
-        <button className='w-full flex justify-center items-center gap-2 bg-gradient-to-r from-[#00DA83] to-[#009BB3] text-white px-4 py-2 mt-6 text-sm rounded-lg cursor-pointer'>
-          <FileText className='w-5'/>
+        <button disabled={loading} className='w-full flex justify-center items-center gap-2 bg-gradient-to-r from-[#00DA83] to-[#009BB3] text-white px-4 py-2 mt-6 text-sm rounded-lg cursor-pointer'>
+          {loading ? (
+            <span className="w-4 h-4 my-1 rounded-full border-2 border-t-transparent animate-spin"></span>
+          ) : (
+            <FileText className='w-5'/>
+          )}
           Review Resume
         </button>
       </form>
