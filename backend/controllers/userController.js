fb85fd12dbe18660bd9f25e04f8498b874dee708
Iamnishankjain <jnishank123@gmail.com>
2025-08-30 14:57:07 +0530

    get published creations api controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -12,3 +12,15 @@ export const getUserCreations = async(req,res)=>{
     res.json({success:false,message:error.message});
   }
 }
+
+
+export const getPublishedCreations = async(req,res)=>{
+  try{
+    const creations = await sql`select * from creations WHERE publish=true ORDER BY created_at DESC`;
+
+    res.json({success:true,creations});
+
+  }catch(error){
+    res.json({success:false,message:error.message});
+  }
+}
