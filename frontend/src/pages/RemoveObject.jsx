0289ee2490936ee343cb307e327a2a0be30c942a
Iamnishankjain <jnishank123@gmail.com>
2025-08-30 18:53:07 +0530

    api to remove object complete

--- frontend/src/pages/RemoveObject.jsx
+++ frontend/src/pages/RemoveObject.jsx
@@ -1,12 +1,49 @@
 import { Scissors, Sparkles } from 'lucide-react';
 import React, { useState } from 'react'
+import axios from "axios";
+import { useAuth } from "@clerk/clerk-react";
+import toast from "react-hot-toast";
+
+axios.defaults.baseURL = import.meta.env.VITE_BASE_URL;
 
 const RemoveObject = () => {
   const [input,setInput] = useState('');
   const [object,setObject] = useState('');
 
+  const [loading, setLoading] = useState(false);
+  const [content, setContent] = useState("");
+
+  const { getToken } = useAuth();
+
   const onSubmitHandler = async (e) =>{
     e.preventDefault();
+    try{
+      setLoading(true);
+      if(object.split(' ').length>1){
+        return toast('Please enter only one object name');
+      }
+
+      const formData = new FormData();
+      formData.append("image", input);
+      formData.append("object", object);
+
+      const { data } = await axios.post(
+        "/api/ai/remove-image-object",
+        formData,
+        {
+          headers: { Authorization: `Bearer ${await getToken()}` },
+        }
+      );
+
+      if (data.success) {
+        setContent(data.content);
+      } else {
+        toast.error(data.message);
+      }
+    }catch (err) {
+      toast.error(err.message);
+    }
+    setLoading(false);
   }
   return (
     <div className='h-full overflow-y-scroll p-6 flex items-start flex-wrap gap-4 text-slate-400'>
@@ -23,8 +60,12 @@ const RemoveObject = () => {
 
         <textarea onChange={(e)=>setObject(e.target.value)} value={object} rows={4} className='w-full p-2 px-3 mt-2 outline-none text-sm rounded-md border border-gray-300' placeholder='e.g.,wtach or spoon,Only single object name' required/>
       
-        <button className='w-full flex justify-center items-center gap-2 bg-gradient-to-r from-[#417DF6] to-[#8E37EB] text-white px-4 py-2 mt-6 text-sm rounded-lg cursor-pointer'>
-          <Scissors className='w-5'/>
+        <button disabled={loading} className='w-full flex justify-center items-center gap-2 bg-gradient-to-r from-[#417DF6] to-[#8E37EB] text-white px-4 py-2 mt-6 text-sm rounded-lg cursor-pointer'>
+          {loading ? (
+            <span className="w-4 h-4 my-1 rounded-full border-2 border-t-transparent animate-spin"></span>
+          ) : (
+            <Scissors className='w-5'/>
+          )}
           Remove Object
         </button>
       </form>
