823e7cce09fe6ce45543c92359f10df2e5cd8bf2
Iamnishankjain <jnishank123@gmail.com>
2025-08-30 15:49:23 +0530

    complete article genration

--- frontend/src/pages/WriteArticle.jsx
+++ frontend/src/pages/WriteArticle.jsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react'
 import axios from 'axios'
 import { useAuth } from '@clerk/clerk-react';
 import toast from 'react-hot-toast';
+import Markdown from 'react-markdown';
 
 axios.defaults.baseURL = import.meta.env.VITE_BASE_URL;
 
@@ -21,7 +22,7 @@ const WriteArticle = () => {
   const [loading,setLoading] =useState(false);
   const [content,setContent] = useState('');
 
-  const {getToken} = useAuth
+  const {getToken} = useAuth();
 
   const onSubmitHandler = async (e) =>{
     e.preventDefault();
@@ -67,8 +68,11 @@ const WriteArticle = () => {
             ))
           }
         </div><br />
-        <button className='w-full flex justify-center items-center gap-2 bg-gradient-to-r from-[#226BFF] to-[#65ADFF] text-white px-4 py-2 mt-6 text-sm rounded-lg cursor-pointer'>
-          <Edit className='w-5'/>
+        <button disabled={loading} className='w-full flex justify-center items-center gap-2 bg-gradient-to-r from-[#226BFF] to-[#65ADFF] text-white px-4 py-2 mt-6 text-sm rounded-lg cursor-pointer'>
+          {
+            loading ? <span className='w-4 h-4 my-1 rounded-full border-2 border-t-transparent animate-spin'></span>
+            : <Edit className='w-5'/>
+          }
           Generate Article
         </button>
       </form>
@@ -79,12 +83,23 @@ const WriteArticle = () => {
           <Edit className='w-5 h-5 text-[#4A7AFF]' />
           <h1 className='text-xl font-semibold'>Generated article</h1>
         </div>
-        <div className='flex-1 flex justify-center items-center'>
-          <div className='text-sm flex flex-col items-center gap-5 text-gray-400'>
-            <Edit className='w-9 h-9'/>
-            <p>Enter a topic and click "Generate article " to get started</p>
+
+        {!content ? (
+          <div className='flex-1 flex justify-center items-center'>
+            <div className='text-sm flex flex-col items-center gap-5 text-gray-400'>
+              <Edit className='w-9 h-9'/>
+              <p>Enter a topic and click "Generate article " to get started</p>
+            </div>
           </div>
-        </div>
+        ) : (
+          <div className='mt-3 h-full overflow-y-scroll text-sm text-slate-600'>
+            <div className='reset-tw'>
+              <Markdown>
+                {content}
+              </Markdown>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   )
