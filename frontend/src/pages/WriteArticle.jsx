af2b30a21e10046a77cc9417cf953204c926174a
Iamnishankjain <jnishank123@gmail.com>
2025-08-30 15:29:06 +0530

    write api to generate article

--- frontend/src/pages/WriteArticle.jsx
+++ frontend/src/pages/WriteArticle.jsx
@@ -1,5 +1,10 @@
 import { Edit, Sparkles } from 'lucide-react'
 import React, { useState } from 'react'
+import axios from 'axios'
+import { useAuth } from '@clerk/clerk-react';
+import toast from 'react-hot-toast';
+
+axios.defaults.baseURL = import.meta.env.VITE_BASE_URL;
 
 const WriteArticle = () => {
 
@@ -13,8 +18,31 @@ const WriteArticle = () => {
 
   const [input,setInput] = useState('');
 
+  const [loading,setLoading] =useState(false);
+  const [content,setContent] = useState('');
+
+  const {getToken} = useAuth
+
   const onSubmitHandler = async (e) =>{
     e.preventDefault();
+    try{
+      setLoading(true)
+      const prompt = `Write an article about ${input} in ${selectedLength.text}`
+
+      const {data} =await axios.post('/api/ai/generate-article',{prompt,length:selectedLength.length},{
+        headers:{Authorization:`Bearer ${await getToken()}`}
+      })
+
+      if(data.success){
+        setContent(data.content);
+      }
+      else{
+        toast.error(data.message);
+      }
+    }catch(err){
+      toast.error(err.message);
+    }
+    setLoading(false);
   }
 
   return (
